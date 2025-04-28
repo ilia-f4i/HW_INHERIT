6eb760e4cee18c21f4e9c4f9fdc64296b881c884
ilia <nu-tro1@mail.ru>
2025-04-28 14:39:52 +0300

    AssertEquals

--- src/main/java/ru/netology/tasks/Todos.java
+++ src/main/java/ru/netology/tasks/Todos.java
@@ -23,9 +23,7 @@ public class Todos {
 
     private Task[] addToArray(Task[] current, Task task) {
         Task[] tmp = new Task[current.length + 1];
-        for (int i = 0; i < current.length; i++) {
-            tmp[i] = current[i];
-        }
+        System.arraycopy(current, 0, tmp, 0, current.length);
         tmp[tmp.length - 1] = task;
         return tmp;
     }
--- src/test/java/ru/netology/tasks/TodosTest.java
+++ src/test/java/ru/netology/tasks/TodosTest.java
@@ -8,7 +8,7 @@ public class TodosTest {
     @Test
     public void shouldAddThreeTasksOfDifferentType() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
-        Epic epic = new Epic(55, new String[] {"Молоко", "Яйца", "Хлеб"});
+        Epic epic = new Epic(55, new String[]{"Молоко", "Яйца", "Хлеб"});
         Meeting meeting = new Meeting(555, "Выкатка версии", "Приложение", "Завтра");
 
         Todos todos = new Todos();
@@ -24,18 +24,22 @@ public class TodosTest {
     @Test
     public void searchShouldReturnOnlyMatchingTasks() {
         Todos todos = new Todos();
-        todos.add(new SimpleTask(1, "Позвонить маме"));
-        todos.add(new Epic(2, new String[]{"Купить молоко", "Купить хлеб"}));
-        todos.add(new Meeting(3, "Обсуждение релиза", "Проект Х", "Понедельник"));
+        Task task1 = new SimpleTask(1, "Позвонить маме");
+        Task task2 = new Epic(2, new String[]{"Купить молоко", "Купить хлеб"});
+        Task task3 = new Meeting(3, "Обсуждение релиза", "Проект Х", "Понедельник");
+        todos.add(task1);
+        todos.add(task2);
+        todos.add(task3);
 
         Task[] actual = todos.search("молоко");
-        assertEquals(1, actual.length);
-        assertEquals(2, actual[0].getId());
+        Task[] expected = {task2};
+        assertArrayEquals(expected, actual);
     }
+
     @Test
     public void shouldAddTasksAndReturnAll() {
         SimpleTask task = new SimpleTask(1, "Позвонить родителям");
-        Epic epic = new Epic(2, new String[] {"Купить молоко", "Купить хлеб"});
+        Epic epic = new Epic(2, new String[]{"Купить молоко", "Купить хлеб"});
         Meeting meeting = new Meeting(3, "Созвон по проекту", "Банковское приложение", "Пятница");
 
         Todos todos = new Todos();
@@ -51,36 +55,42 @@ public class TodosTest {
     @Test
     public void shouldFindTasksByQuery_MatchOne() {
         Todos todos = new Todos();
-        todos.add(new SimpleTask(1, "Позвонить маме"));
-        todos.add(new Epic(2, new String[] {"Сходить в магазин", "Купить молоко"}));
-        todos.add(new Meeting(3, "Обсуждение релиза", "Проект Х", "Понедельник"));
+        Task task1 = new SimpleTask(1, "Позвонить маме");
+        Task task2 = new Epic(2, new String[]{"Сходить в магазин", "Купить молоко"});
+        Task task3 = new Meeting(3, "Обсуждение релиза", "Проект Х", "Понедельник");
+        todos.add(task1);
+        todos.add(task2);
+        todos.add(task3);
 
         Task[] result = todos.search("молоко");
-
-        assertEquals(1, result.length);
-        assertEquals(2, result[0].getId());
+        Task[] expected = {task2};
+        assertArrayEquals(expected, result);
     }
 
     @Test
     public void shouldFindTasksByQuery_MatchMultiple() {
         Todos todos = new Todos();
-        todos.add(new SimpleTask(1, "Позвонить маме"));
-        todos.add(new Epic(2, new String[] {"Купить молоко", "Купить хлеб"}));
-        todos.add(new Meeting(3, "Обсуждение молоко", "Проект Х", "Понедельник")); // 👈 исправлено
+        Task task1 = new SimpleTask(1, "Позвонить маме");
+        Task task2 = new Epic(2, new String[]{"Купить молоко", "Купить хлеб"});
+        Task task3 = new Meeting(3, "Обсуждение молоко", "Проект Х", "Понедельник");
+        todos.add(task1);
+        todos.add(task2);
+        todos.add(task3);
 
         Task[] result = todos.search("молоко");
-
-        assertEquals(2, result.length);
+        Task[] expected = {task2, task3}; // Порядок зависит от добавления
+        assertArrayEquals(expected, result);
     }
+
     @Test
     public void shouldReturnEmptyIfNoMatches() {
         Todos todos = new Todos();
         todos.add(new SimpleTask(1, "Позвонить маме"));
-        todos.add(new Epic(2, new String[] {"Сходить в магазин"}));
+        todos.add(new Epic(2, new String[]{"Сходить в магазин"}));
         todos.add(new Meeting(3, "Совещание", "Проект Х", "Понедельник"));
 
         Task[] result = todos.search("отпуск");
-
-        assertEquals(0, result.length);
+        Task[] expected = {};
+        assertArrayEquals(expected, result);
     }
 }
